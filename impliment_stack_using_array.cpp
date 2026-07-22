b8f8c54b0bbdfd89060913d688b5778236ea9df2
salma10102005 <ummey.salma1110@gmail.com>
2026-07-22 20:01:08 +0600

    add

---
+++ impliment_stack_using_array.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class mystack
+{
+    public :
+        vector<int>v;
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+
+int main()
+{
+    mystack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout<< st.top() << endl;
+    st.pop(); // shbcheye uporer value ta delet hoye jabe
+    cout<< st.top() << endl;
+    if(st.empty() == false)
+         cout<< st.top() << endl;
+
+
+
+    return 0;
+}
